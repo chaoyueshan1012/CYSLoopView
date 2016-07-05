4a9fa4cae7a698bf53d1a1e86fc8ccb7e33d3dc7
chaoyueshan <chaoyueshan@u51.com>
2016-07-05 16:31:53 +0800

    release 10.0

---
+++ CYSLoopView/LoopView.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYSLoopViews
+//
+//  Created by chaoyueshan on 16/7/5.
+//  Copyright © 2016年 Giles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoopView : UIView
+
+
+@end
+
---
+++ CYSLoopView/LoopView.m
@@ -0,0 +1,15 @@
+//
+//  ViewController.m
+//  CYSLoopViews
+//
+//  Created by chaoyueshan on 16/7/5.
+//  Copyright © 2016年 Giles. All rights reserved.
+//
+
+#import "LoopView.h"
+
+
+@implementation LoopView
+
+
+@end
